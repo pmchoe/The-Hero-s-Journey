de4887b05bf7935e2f46ed8741311e16f944c0fe
Philip Choe <pmchoe@ucsc.edu>
2023-05-11 00:25:47 -0700

    added color for background

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 let config = {
-    type: Phaser.CANVAS,
-    width: 1200,
-    height: 600,
-    scene: [ Menu, Play ]
-}
+    type: Phaser.AUTO,
+    width: 1280,
+    height: 960,
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: { y: 1000 },
+            debug: false,
+        },
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update,
+    },
+};
 
 let game = new Phaser.Game(config);
+
+// set UI sizes
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,6 +4,12 @@ class Play extends Phaser.Scene {
     }
 
     create() {
-        this.add.text(20, 20, "The Hero's Journey Play")
+        // green UI background
+        this.add.rectangble(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
+        //white borders
+        this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
     }
 }

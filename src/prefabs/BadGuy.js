a0990ab9e8ec0ea2877591720502b98a9d7e684a
Philip Choe <pmchoe@ucsc.edu>
2023-05-12 16:14:38 -0700

    trying to make enemy move left

--- src/prefabs/BadGuy.js
+++ src/prefabs/BadGuy.js
@@ -1,11 +1,11 @@
 // bad guy, aka "Enemy" prefab
 class BadGuy extends Phaser.GameObjects.Sprite {
-    constructor(scene, x, y, texture, frame, scrollSpeed) {
+    constructor(scene, x, y, texture, frame) {
         super(scene, x, y, texture, frame);
 
         // THIS NEEDS TO BE RANDOMIZED BETWEEN ENEMY 9-12 SPRITES
         scene.add.existing(this);   // add to existing, displayList, updateList
-        this.moveSpeed = scrollSpeed; // pixels per frame, matches background speed
+        this.moveSpeed = 8; // pixels per frame, matches background speed
     }
 
     update() {

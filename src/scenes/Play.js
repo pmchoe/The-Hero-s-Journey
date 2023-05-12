2de61157a9aa4e3da3eb583b728dd5246bd86b77
Philip Choe <pmchoe@ucsc.edu>
2023-05-12 02:04:10 -0700

    I DID THIS.PRELOAD INSTEAD OF THIS.LOAD OMG

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -5,15 +5,15 @@ class Play extends Phaser.Scene {
 
     preload() {
         // load images/tile sprites
-        this.preload.image('background', './assets/background.png');
-        this.preload.image('textureAtlas', './assets/textureAtlas.png');
+        this.load.image('background', './assets/background.png');
+        this.load.image('textureAtlas', './assets/textureAtlas.png');
         /*
-        this.preload.audio('playBGM', './assets/playBGM.mp3');
-        this.preload.audio('menuBGM', './assets/menuBGM.mp3');
-        this.preload.audio('gameoverBGM', './assets/gameoverBGM.mp3);
-        this.preload.audio('select', './assets/select.mp3');
-        this.preload.audio('hooray', './assets/hooray.mp3');
-        this.preload.audio('oopsie', './assets/oopsie.mp3');
+        this.load.audio('playBGM', './assets/playBGM.mp3');
+        this.load.audio('menuBGM', './assets/menuBGM.mp3');
+        this.load.audio('gameoverBGM', './assets/gameoverBGM.mp3);
+        this.load.audio('select', './assets/select.mp3');
+        this.load.audio('hooray', './assets/hooray.mp3');
+        this.load.audio('oopsie', './assets/oopsie.mp3');
         */ 
     }
 

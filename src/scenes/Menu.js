9fbc629483651f6b852799013c5decc3b26e5d52
Philip Choe <pmchoe@ucsc.edu>
2023-05-12 17:01:39 -0700

    title in the middle and bolder

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -16,7 +16,7 @@ class Menu extends Phaser.Scene {
         this.background = this.add.image(0, 0, 'background').setOrigin(0, 0); 
         
         // title text
-        this.add.text(500, 860, "The Hero's Journey", { fontSize: '32px', fill: '#3d3d3d' }).setOrigin(0.5);
+        this.add.text(640, 480, "The Hero's Journey", {fontFamily: 'Arial', fontSize: '32px', fill: '#3d3d3d', fontStyle: 'bold'}).setOrigin(0.5);
 
         // spawns static hero
         /*this.player = new Hero(this, 300, 650, 'textureAtlas', 'textureAtlasSplit-0.png').setOrigin(1, 0.5);

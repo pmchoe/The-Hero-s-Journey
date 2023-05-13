0c84078a8309384147373fc4c0358c8b5d98810c
Philip Choe <pmchoe@ucsc.edu>
2023-05-13 13:02:46 -0700

    testing the transparency/opaque for icons

--- src/prefabs/Icons.js
+++ src/prefabs/Icons.js
@@ -10,25 +10,25 @@ class Icons extends Phaser.GameObjects.Sprite {
         // highlights icon with W, A, S, D presses
         // also de-highlights any other button
         if(Phaser.Input.Keyboard.JustDown(keyW)) {
-            this.iconW.setAlpha(1);
-            this.iconA.setAlpha(0.3);
-            this.iconS.setAlpha(0.3);
-            this.iconD.setAlpha(0.3);
+            iconW.setAlpha(1);
+            iconA.setAlpha(0.3);
+            iconS.setAlpha(0.3);
+            iconD.setAlpha(0.3);
         } else if(Phaser.Input.Keyboard.JustDown(keyA)) {
-            this.iconW.setAlpha(0.3);
-            this.iconA.setAlpha(1);
-            this.iconS.setAlpha(0.3);
-            this.iconD.setAlpha(0.3);
+            iconW.setAlpha(0.3);
+            iconA.setAlpha(1);
+            iconS.setAlpha(0.3);
+            iconD.setAlpha(0.3);
         } else if(Phaser.Input.Keyboard.JustDown(keyS)) {
-            this.iconW.setAlpha(0.3);
-            this.iconA.setAlpha(0.3);
-            this.iconS.setAlpha(1);
-            this.iconD.setAlpha(0.3);
+            iconW.setAlpha(0.3);
+            iconA.setAlpha(0.3);
+            iconS.setAlpha(1);
+            iconD.setAlpha(0.3);
         } else if(Phaser.Input.Keyboard.JustDown(keyW)) {
-            this.iconW.setAlpha(0.3);
-            this.iconA.setAlpha(0.3);
-            this.iconS.setAlpha(0.3);
-            this.iconD.setAlpha(1);
+            iconW.setAlpha(0.3);
+            iconA.setAlpha(0.3);
+            iconS.setAlpha(0.3);
+            iconD.setAlpha(1);
         }
     }
 }

7d75650f587756c53d34e3b264d91b367b02f937
Philip Choe <pmchoe@ucsc.edu>
2023-05-13 17:06:24 -0700

    please let this animation sprite shift work

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -31,13 +31,13 @@ class Play extends Phaser.Scene {
         this.anims.create({
             key: 'hero_running',
             frames: [
-                {key: 'textureAtlas', frame: 'textureAtlasSplit-2.png', origin: {x: 1, y: 0.5}},
-                {key: 'textureAtlas', frame: 'textureAtlasSplit-3.png', origin: {x: 1, y: 0.5}},
-                {key: 'textureAtlas', frame: 'textureAtlasSplit-4.png', origin: {x: 1, y: 0.5}},
+              { key: 'textureAtlas', frame: 'textureAtlasSplit-2.png', frameWidth: 64, frameHeight: 64, },
+              { key: 'textureAtlas', frame: 'textureAtlasSplit-3.png', frameWidth: 64, frameHeight: 64, },
+              { key: 'textureAtlas', frame: 'textureAtlasSplit-4.png', frameWidth: 64, frameHeight: 64, },
             ],
             frameRate: 10,
-            repeat: -1  // loops infinitely
-        });
+            repeat: -1
+          });          
         this.player = new Hero(this, 300, 650, 'textureAtlas', 'textureAtlasSplit-1.png').setOrigin(1, 0.5);
         this.player.anims.play('hero_running', true).setOrigin(1, 0.5);   // animates hero running
 
